5368847e912afb966cb87232dca4002dcd9a1c71
asamokhina94 <cdfiY#16Z>
2020-04-15 14:01:29 +0300

    Initial commit

---
+++ src/Task_5.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Task_5 {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input a number: ");
+        double num = in.nextDouble();
+        int x = 1;
+        System.out.println(num + " * " + x + " = "  + (num*x));
+        x++;
+        System.out.println(num + " * " + x + " = "  + (num*x));
+        x++;
+        System.out.println(num + " * " + x + " = "  + (num*x));
+        x++;
+        System.out.println(num + " * " + x + " = "  + (num*x));
+        x++;
+        System.out.println(num + " * " + x + " = "  + (num*x));
+        x++;
+        System.out.println(num + " * " + x + " = "  + (num*x));
+        x++;
+        System.out.println(num + " * " + x + " = "  + (num*x));
+        x++;
+        System.out.println(num + " * " + x + " = "  + (num*x));
+        x++;
+        System.out.println(num + " * " + x + " = "  + (num*x));
+        x++;
+        System.out.println(num + " * " + x + " = "  + (num*x));
+    }
+}
